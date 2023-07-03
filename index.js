a985f76d43e65acdd1c14bcba0e08631ff27324b
Rizky Yugo Pratama <rizkyyugo123@gmail.com>
2023-07-03 20:32:14 +0700

    test

---
+++ index.js
@@ -0,0 +1,80 @@
+const path =require('path')
+const express = require('express');
+const methodOverride = require('method-override')
+const mongoose =require('mongoose')
+const app = express()
+
+
+// Models
+const Product = require('./models/product')
+//connect to mongodb
+mongoose.connect('mongodb://127.0.0.1/shop_db').then((res)=>{
+    console.log('Terhubung')
+}).catch((err)=>{
+    console.log(err)
+})
+app.set('views',path.join(__dirname,'views'))
+app.set('view_engine','ejs')
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+app.get('/',(req,res)=>{
+    res.send('helloq world')
+})
+
+app.get('/products',async (req,res)=>{
+    const {category} = req.query
+    if(category){
+        const products = await Product.find({category})
+        res.render('index.ejs',{
+            products,
+            category
+        })}else{
+    const products = await Product.find()
+    res.render('index.ejs',{
+        products,
+        category:'ALL'
+    })
+
+}
+})
+
+app.get('/products/create',(req,res)=>{
+    res.render('create.ejs')
+})
+app.get('/products/:id',async(req,res)=>{
+    const {id} =req.params
+    const products = await Product.findById(id)
+    res.render('show.ejs',{
+        products
+    })
+})
+
+app.get('/products/:id/edit',async(req,res)=>{
+    const {id} =req.params
+    const products = await Product.findById(id)
+    res.render('edit.ejs',{
+        products
+    })
+})
+app.post('/products',async(req,res)=>{
+    const product = new Product(req.body)
+    await product.save()
+    res.redirect(`/products/${product._id}`)
+})
+
+app.put('/products/:id',async(req,res)=>{
+    const {id} = req.params
+    const products= await Product.findByIdAndUpdate(id,req.body,{
+        runValidators:true,
+    })
+    res.redirect(`/products/${products._id}`)
+})
+app.delete('/products/:id',async(req,res)=>{
+    const {id}=req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+})
+
+app.listen(3000,()=>{
+    console.log('Shop app Listening on http://127.0.0.1:3000')
+})
